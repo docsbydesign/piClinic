3ae4cbd2ade5779cbe48850a0fc250dccaf20080
Bob Watson <rbwatson@uw.edu>
2019-04-03 16:22:45 -0400

    Updated copyright strings

--- www/html/visitUiStrings.php
+++ www/html/visitUiStrings.php
@@ -19,12 +19,6 @@
  *	If not, see <http://www.gnu.org/licenses/>.
  *
  */
-/**
- * Created by PhpStorm.
- * User: BobW
- * Date: 3/6/2018
- * Time: 5:12 PM
- */
 /* These strings are used in the UI and must be included after the language specific strings */
 /* Define staff position values */
 require_once dirname(__FILE__).'/./uitext/visitUiStringsText.php';
