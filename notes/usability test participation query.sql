0583505a89e8976345842a713c679a4c49dcdf43
Bob Watson <rbwatson@uw.edu>
2019-04-24 18:02:43 -0400

    Updated report queries

---
+++ notes/usability test participation query.sql
@@ -0,0 +1,25 @@
+-- select and count the participants registered from the usability test
+-- 
+SELECT 
+    TRIM(SUBSTR(`a`.`name`, POSITION(' ' in `a`.`name`))) as `lastName`,
+	`a`.`name`,
+    MIN(`a`.`commentDate`) AS `firstTest`, 
+    MAX(`a`.`commentDate`) AS `lastTest`, 
+    COUNT(`a`.`name`) AS `participationCount`,
+    (CASE
+    	WHEN COUNT(`a`.`name`) > 4
+        THEN
+        	20
+        ELSE 
+	     	COUNT(`a`.`name`)* 5 
+    END) AS `extraCredit` 
+  FROM (
+     SELECT `b`.`commentDate` as `commentDate`,`b`.`commentString` as `commentString`, TRIM(SUBSTR(`b`.`commentString`,1,POSITION('-' IN `b`.`commentString`)-1)) as `name` FROM (
+        SELECT `commentDate`, 
+            REPLACE(REPLACE(`commentText`,'<',''),'>','') as `commentString`
+          FROM `comment`
+          WHERE `commentText` like '%finished%') AS `b`      
+    WHERE 1) AS `a`
+WHERE 1
+GROUP BY `a`.`name`
+ORDER BY `lastName`;
