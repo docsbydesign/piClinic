45c2b908a16c18b83475e2946f7b6897d5a859ed
Bob Watson <bobw@docsbydesign.com>
2019-05-18 07:36:29 -0600

    Replaced roatan clinic script

---
+++ sql/loadRoatanClinics.sql
@@ -0,0 +1,7 @@
+USE `piclinic`;
+--
+TRUNCATE TABLE `clinic`;
+INSERT INTO `clinic` (`thisClinic`,`publicID`,`typeCode`,`careLevel`,`longName`,`shortName`,`currency`,`address1`,`address2`,`clinicNeighborhood`,`clinicCity`,`clinicState`,`clinicRegion`,`clinicDirector`,`clinicService`,`modifiedDate`,`createdDate`) VALUES 
+		("0",NULL,"UAPS","Primario","Consolation Clinic","Consolation Clinic","HNL",NULL,NULL,"Consolation Bight","Roatan","Islas de la Bahia","11","Aleynska Grant","Outpatient",NOW(),NOW()),
+		("0","001348","UAPS","Primario","Vasijas de Misericordia","Vasijas de Misericordia","HNL",NULL,NULL,"French Cay Harbour","Roatan","Islas de la Bahia","11","Dra. Sarai Raudales","Outpatient",NOW(),NOW()),
+		("0",NULL,"UAPS","Primario","Oak Ridge Community Health Association","Oak Ridge Community Health Association","HNL",NULL,NULL,"Oak Ridge","Roatan","Islas de la Bahia","11","Anneth Cooper","Outpatient",NOW(),NOW());
