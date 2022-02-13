6fe2c3f02a9afab9b6754ee8a04746a580e80d0c
Bob Watson <github@bobtalkstech.com>
2022-02-12 17:18:11 -0800

    Add missing copyright notice

--- www/html/index.php
+++ www/html/index.php
@@ -1,5 +1,28 @@
 <?php
-
+/*
+ *
+ *	Copyright (c) 2019, Robert B. Watson
+ *
+ *	This file is part of the piClinic Console.
+ *
+ *  piClinic Console is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  piClinic Console is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with piClinic Console software at https://github.com/docsbydesign/piClinic/blob/master/LICENSE.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/* redirect to generic access attempts to documentation page to prevent accidental
+ * access to online systems
+ */
 header("Location: https://piclinic.org", true, 301);
 
 exit;
